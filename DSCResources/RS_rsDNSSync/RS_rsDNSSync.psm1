863d3ce5b822e4e5e48956bdaec6a7215aa5c45c
Brandon VanDermark <brandon.vandermark@rackspace.com>
2015-04-16 20:34:18 -0500

    Initial Commit - New DSC Resource Module with Test-TargetResource written

---
+++ DSCResources/RS_rsDNSSync/RS_rsDNSSync.psm1
@@ -0,0 +1,37 @@
+﻿. (Get-rsSecrets)
+function Get-TargetResource{
+    
+}
+function Test-TargetResource{
+    param(
+        $AdapterName = "Public",
+        [ValidateSet("CloudServer","CloudDNS")]
+        $DNSProvider = "CloudServer",
+        [Parameter(Mandatory = $true)]
+        $ZoneName
+    )
+
+    $cloudRecords = Get-rsCloudServersInfo | select name,@{n="RecordData";e={$_.addresses.$AdapterName.addr}}
+
+    switch($DNSProvider){
+        CloudServer{
+            if(!(Get-WindowsFeature DNS).Installed){Return $false}
+            elseif((Get-DnsServerZone $ZoneName -ErrorAction SilentlyContinue) -eq $null){Return $false}
+            else{
+                $localRecords = @()
+                foreach($record in Get-DnsServerResourceRecord -ZoneName showpitch | ? RecordType -eq A){
+                    $recordEnum = New-Object psobject -Property @{'name'=$record.HostName;'RecordData'=$record.RecordData.IPv4Address.IPAddressToString}
+                    $localRecords += $recordEnum
+                }
+                if($localRecords -ne $cloudRecords){Return $false}
+                else{Return $true}
+            }
+        }
+        CloudDNS{
+
+        }
+    }
+}
+function Set-TargetResource{
+
+}
