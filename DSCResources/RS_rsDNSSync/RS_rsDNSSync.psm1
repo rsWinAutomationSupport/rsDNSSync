c86019e5b995e371a230d9a5b23d7f3787ef7eaf
Brandon VanDermark <brandon.vandermark@rackspace.com>
2015-04-17 21:52:30 -0500

    Finished building Set-TargetResource and Get-TargetResouce

--- DSCResources/RS_rsDNSSync/RS_rsDNSSync.psm1
+++ DSCResources/RS_rsDNSSync/RS_rsDNSSync.psm1
@@ -1,6 +1,43 @@
 ﻿. (Get-rsSecrets)
 function Get-TargetResource{
-    
+    [OutputType([hashtable])]
+    param(
+        [Parameter(Mandatory = $true)]
+        $AdapterName = "Public",
+        [ValidateSet("CloudServer","CloudDNS")]
+        $DNSProvider = "CloudServer",
+        [Parameter(Mandatory = $true)]
+        $ZoneName
+    )
+
+    switch($DNSProvider){
+        CloudServer{
+            $localDNSPresent = (Get-WindowsFeature DNS).Installed
+            if($localDNSPresent -eq $true){
+                if((Get-DnsServerZone $ZoneName -ErrorAction SilentlyContinue) -ne $null){
+                    $localRecords = @()
+                    foreach($record in Get-DnsServerResourceRecord -ZoneName $ZoneName | ? RecordType -eq A){
+                        $recordEnum = New-Object psobject -Property @{'name'=$record.HostName;'RecordData'=$record.RecordData.IPv4Address.IPAddressToString}
+                        $localRecords += $recordEnum
+                    }
+                }
+            }
+        }
+        CloudDNS{
+
+        }
+    }
+    else{$localRecords = $null}
+    $cloudRecords = Get-rsCloudServersInfo | select name,@{n="RecordData";e={$_.addresses.$AdapterName.addr}}
+
+    @{
+        "DNSProvider" = $DNSProvider
+        "localDNSPresent" = $localDNSPresent
+        "ZoneName" = $ZoneName
+        "TargetAdapter" = $AdapterName
+        "CloudServers" = $cloudRecords
+        "LocalRecords" = $localRecords
+    }
 }
 function Test-TargetResource{
     param(
@@ -19,7 +56,7 @@ function Test-TargetResource{
             elseif((Get-DnsServerZone $ZoneName -ErrorAction SilentlyContinue) -eq $null){Return $false}
             else{
                 $localRecords = @()
-                foreach($record in Get-DnsServerResourceRecord -ZoneName showpitch | ? RecordType -eq A){
+                foreach($record in Get-DnsServerResourceRecord -ZoneName $ZoneName | ? RecordType -eq A){
                     $recordEnum = New-Object psobject -Property @{'name'=$record.HostName;'RecordData'=$record.RecordData.IPv4Address.IPAddressToString}
                     $localRecords += $recordEnum
                 }
@@ -33,5 +70,44 @@ function Test-TargetResource{
     }
 }
 function Set-TargetResource{
+    param(
+        $AdapterName = "Public",
+        [ValidateSet("CloudServer","CloudDNS")]
+        $DNSProvider = "CloudServer",
+        [Parameter(Mandatory = $true)]
+        $ZoneName
+    )
 
+    $cloudRecords = Get-rsCloudServersInfo | select name,@{n="RecordData";e={$_.addresses.$AdapterName.addr}}
+
+    switch($DNSProvider){
+        CloudServer{
+            if(!(Get-WindowsFeature DNS).Installed){
+                Install-WindowsFeature DNS -IncludeManagementTools
+            }
+            if((Get-DnsServerZone $ZoneName -ErrorAction SilentlyContinue) -eq $null){
+                Add-DnsServerPrimaryZone -Name $ZoneName -ZoneFile ($ZoneName + ".dns")
+            }
+
+            $localRecords = @()
+            foreach($record in Get-DnsServerResourceRecord -ZoneName showpitch | ? RecordType -eq A){
+                $recordEnum = New-Object psobject -Property @{'name'=$record.HostName;'RecordData'=$record.RecordData.IPv4Address.IPAddressToString}
+                $localRecords += $recordEnum
+            }
+            
+            foreach($record in $cloudRecords){
+                if($localRecords -notcontains ($record.name -and $record.RecordData)){
+                    Add-DnsServerResourceRecord -ZoneName $ZoneName -A -Name $record.name -IPv4Address $record.RecordData
+                }
+            }
+            foreach($record in $localRecords){
+                if($cloudRecords -notcontains ($record.name -and $record.RecordData)){
+                    Remove-DnsServerResourceRecord -ZoneName $ZoneName -RRType A -Name $record.name -RecordData $record.RecordData
+                }
+            }
+        }
+        CloudDNS{
+
+        }
+    }
 }
